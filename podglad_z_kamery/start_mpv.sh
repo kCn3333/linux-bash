9a880463741c8a8aca1582f994906bf8c47f0e57
Pawel Jacek Kwiecien <kwiecien@o2.pl>
2024-12-15 12:31:01 +0100

    Stop tracking folders

--- podglad_z_kamery/start_mpv.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Strumień RTSP
-RTSP_URL="rtsp://admin:69DupaDupa@192.168.0.64:554/Streaming/Channels/101"
-
-# Uruchomienie mpv w tle
-mpv "$RTSP_URL"
-#nohup mpv "$RTSP_URL" > /dev/null 2>&1
-echo "Stream RTSP uruchomiony w tle."
